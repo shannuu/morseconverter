08780048055b2d1136bf46b8d63990b322c29ba7
Kushal <64453862+shannuu@users.noreply.github.com>
2022-02-10 22:24:47 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,21 @@
-pkg install git
+repnamer(){
 cd
-rm -rf morseconverter
-git clone https://github.com/shannuu/morseconverter
-
+list=$(ls)
+count=$(echo $list | wc -w)
+rcount=$(( $count + 1 ))
+var=1
+until [[ $var == $rcount ]]; do
+rep=$(echo $list | cut -d " " -f $var)
+frep=$(echo $rep | tr '[:lower:]' '[:upper:]')
+if [[ $frep == "MORSECONVERTER" ]]; then
+break
+exit
+fi
+((var++))
+done
+}
+repnamer
+rm -rf $rep > /dev/null 2>&1
+pkg install git -y > /dev/null 2>&1
+git clone https://github.com/Shannuu/MorseConverter > /dev/null 2>&1
+echo "Done"
